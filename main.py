91c4eca4bef222433f8215dba4b0a564dc9575a1
ndhuru <144291089+ndhuru@users.noreply.github.com>
2023-11-10 22:13:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,6 @@ from flask import Flask, request, jsonify
 from adafruit_motorkit import MotorKit
 import time
 
-#motor kit instance
 kit = MotorKit(0x40)
 #flask app
 app = Flask(__name__)
@@ -98,16 +97,22 @@ def control():
     #implement control logic based on received command
     if command == 'forward':
         forward(2.6)
+      #forward statement, i set it to 2.6 as an example
     elif command == 'backward':
         backward(2.6)
+      #backward statement, i set it to 2.6 as an example
     elif command == 'left':
         left(2.6)
+      #left statement, i set it to 2.6 as an example
     elif command == 'right':
         right(2.6)
+      #right statement, i set it to 2.6 as an example
     elif command == 'stop':
         stop()
+      #stop statement
     elif command == 'play':
         play()
+      #play statement, I just had it run something
     else:
         return jsonify({'status': 'error', 'message': 'Invalid command'})
     return jsonify({'status': 'success', 'message': 'Command executed'})
@@ -115,16 +120,54 @@ def control():
 #define functions for movement commands
 def forward(amount_of_time_to_run):
     #adjust motor throttle for forward movement
+    #notice how they amde the motors backwards
+    #-0.5 and 0.64 to move forward
     kit.motor1.throttle = -0.5
     kit.motor2.throttle = 0.64
     time.sleep(amount_of_time_to_run)
     kit.motor1.throttle = 0.0
     kit.motor2.throttle = 0.0
 
+def backward(amount_of_time_to_run):
+  #same confusion
+    kit.motor1.throttle = 0.7
+    kit.motor2.throttle = -0.7
+    time.sleep(amount_of_time_to_run)
+    kit.motor1.throttle = 0.0
+    kit.motor2.throttle = 0.0
+
+def left(amount_of_time_to_run):
+  #left, 
+  #now need to negate both motor throttles for some reason instead of negating just one...
+    kit.motor1.throttle = -0.64
+    kit.motor2.throttle = -0.64
+    time.sleep(amount_of_time_to_run)
+    kit.motor1.throttle = 0.0
+    kit.motor2.throttle = 0.0
+
+def right(amount_of_time_to_run):
+  #right, now they're both positive?
+    kit.motor1.throttle = 0.64
+    kit.motor2.throttle = 0.64
+    time.sleep(amount_of_time_to_run)
+    kit.motor1.throttle = 0.0
+    kit.motor2.throttle = 0.0
+
+def stop():
+  #straightforward stop function
+    kit.motor1.throttle = 0.0
+    kit.motor2.throttle = 0.0
+
+def play():
+  #set play to move
+    kit.motor1.throttle = 0.5
+    kit.motor2.throttle = 0.5
+
 #define similar functions for backward, left, right, stop, and play commands
 #(backward, left, right, stop, play functions follow a similar structure)
 
 #run the Flask app
 if __name__ == '__main__':
-    #start the app on a specific host and port
+    #start the app on a specific host and port, I don't know how the port part works though
+    #will research ports more
     app.run(host='0.0.0.0', port=4444)
